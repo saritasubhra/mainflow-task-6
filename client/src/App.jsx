40fce15172dfb7ae02167e97dfacd867798cad2f
saritasubhra <saritasubhra1@gmail.com>
2025-02-06 19:19:06 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,50 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
+import { useAuth } from "./contexts/AuthContext";
+import AdminDashboard from "./pages/AdminDashboard";
+import Category from "./pages/Category";
+import CartPage from "./pages/CartPage";
+
 function App() {
-  return <div>App</div>;
+  const { auth } = useAuth();
+
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="absolute inset-0">
+            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+          </div>
+        </div>
+
+        <div className="relative z-50 pt-20">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/login"
+              element={auth ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={auth ? <Navigate to="/" /> : <SignUp />}
+            />
+            <Route path="/dashboard" element={<AdminDashboard />} />
+            <Route path="/category/:category" element={<Category />} />
+            <Route
+              path="/cart"
+              element={auth ? <CartPage /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </div>
+        <Toaster />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
