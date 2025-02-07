60482701fbbded41747b5b270c218a5803f70fe1
saritasubhra <saritasubhra1@gmail.com>
2025-02-07 19:28:49 +0530

    pages

---
+++ client/src/pages/CartPage.jsx
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import EmptyCartUI from "../components/EmptyCartUI";
+import { useCart } from "../contexts/CartContext";
+import CartItem from "../components/CartItem";
+import { useEffect } from "react";
+
+function CartPage() {
+  const { cart, fetchCartItems } = useCart();
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+  return (
+    <div className="py-8 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+          <motion.div
+            className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl"
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            {!cart.length ? (
+              <EmptyCartUI />
+            ) : (
+              <div className="space-y-6">
+                {cart.map((item) => (
+                  <CartItem key={item._id} item={item} />
+                ))}
+              </div>
+            )}
+          </motion.div>
+
+          {/* {cart.length > 0 && (
+            <motion.div
+              className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full"
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              <OrderSummary />
+              <GiftCouponCard />
+            </motion.div>
+          )} */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartPage;
